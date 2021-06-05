b7cc2ec038add6b59a23a69df4eedfd3516dd2ea
ashparra97 <ashparra97@arizona.edu>
2021-06-05 07:59:00 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,26 @@
 
 #import modules
 import numpy as np  
+from flask import Flask, jsonify 
+
+
+# Flask set up
+app = Flask(__name__)
+
+# Route 1: Home 
+@app.route("/")
+def home(): 
+    return "Hi"
+
+# Route 1: Home
+
+
+
+# Route 1: Home
+
+
+
+
+# Route 1: Home
+
+    
