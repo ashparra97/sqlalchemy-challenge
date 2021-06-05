6e92fb66d24c942a78d8f22b732aa058271a1800
ashparra97 <ashparra97@arizona.edu>
2021-06-05 08:01:47 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 # Flask Challenge
 
 #import modules
-import numpy as np  
 from flask import Flask, jsonify 
 
 
@@ -11,17 +10,27 @@ app = Flask(__name__)
 # Route 1: Home 
 @app.route("/")
 def home(): 
-    return "Hi"
+    return home
 
-# Route 1: Home
-
-
-
-# Route 1: Home
+# Route 2: Precipitation
+@app.route("/api/v1.0/precipitation")
 
 
 
+# Route 1: Stations
+@app.route("/api/v1.0/stations")
 
-# Route 1: Home
 
+
+
+# Route 1: Dates and temps observations
+@app.route("/api/v1.0/tobs")
+
+
+
+# Route 1: Temperatures
+@app.route("/api/v1.0/<start")
+
+
+@app.route("/api/v1.0/<start>/<end")
     
